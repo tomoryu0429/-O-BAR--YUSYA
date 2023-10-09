5bb181ba01b7fe212317dd3cbde1c439cef996a6
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-10-09 13:01:30 +0900

    [add] ステージ格納用リスト作成

--- Assets/Yoshida/Scripts/StageManager.cs
+++ Assets/Yoshida/Scripts/StageManager.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<int> _stages = new();
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
         
     }
