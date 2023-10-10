d642ce53fe097f085d3d13343c5ca47982d94966
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-10-10 18:11:05 +0900

    [add] ランダムにステージ抽選する機能作成

--- Assets/Yoshida/Scripts/StageManager.cs
+++ Assets/Yoshida/Scripts/StageManager.cs
@@ -1,17 +1,39 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class StageManager : MonoBehaviour
 {
-    [SerializeField] private List<int> _stages = new();
+    /// <summary> 戦闘シーン名格納リスト</summary>
+    [SerializeField] [Header("戦闘シーン名")] private List<string> _stageNames = new();
+    /// <summary> ステージをランダムに選択の数字を格納するリスト</summary>
+    private List<int> _stageNums = new();
+    public static StageManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+            Destroy(gameObject);
+    }
+
     private void Start()
     {
-        
+        StageRandom().ForEach(x => print($"{_stageNames[x]} : {x}"));
     }
-    
-    private void Update()
+    /// <summary> 格納したステージをランダムに並べ替える</summary>
+    /// <returns>ランダムに選べ変えたリストを返す</returns>
+    private List<int> StageRandom()
     {
-        
+        for (var i = 0; i < _stageNames.Count; i++)
+        {
+            _stageNums.Add(i);
+        }
+        return _stageNums.OrderBy(x => Guid.NewGuid()).ToList();
     }
 }
