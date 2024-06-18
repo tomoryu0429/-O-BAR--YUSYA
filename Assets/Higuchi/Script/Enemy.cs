aa99d72db8aff7b11d85db73e5335dc1d681b9b3
HiguchiHaruya <tongkouyangye@gmail.com>
2024-06-18 18:15:08 +0900

    enemyのScriptableObject作成

---
+++ Assets/Higuchi/Script/Enemy.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "newEnemy",menuName = "EnemyScriptableObject/Enemy")]
+public class Enemy : ScriptableObject
+{
+    public string EnemyNeme; //“G‚Ì–¼‘O
+    public int health; //“G‚ÌHP
+    public string description; //“G‚Ìà–¾
+    public int power; //“G‚ÌUŒ‚—Í
+}
