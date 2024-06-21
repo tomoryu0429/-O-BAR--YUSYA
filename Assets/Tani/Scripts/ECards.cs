4647cf3eafafe570711f6fbd2de4dc399a210783
Route-Paradise <lappyparadise1341@gmail.com>
2024-06-21 18:11:15 +0900

    ghfc

---
+++ Assets/Tani/Scripts/ECards.cs
@@ -0,0 +1,18 @@
+﻿// EnumBuild.csで生成
+
+namespace AutoEnum
+{
+	[System.Serializable]
+	public enum ECards
+	{
+		None = -1,
+		fish_sozai_card,
+		mash_sozai_card1,
+		meet_sozai_card,
+		onion_sozai_card3,
+		rice_sozai_card4,
+		tomato_sozai_card2,
+		zer_sozai_card5,
+		Max,
+	};
+}
